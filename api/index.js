93c24c3167acad9072dd8b26b3c842093097fecb
trbsystemdev-blip <trb.system.dev@gmail.com>
2026-06-29 22:33:08 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-password']
 }));
 app.use(express.json());
 app.use(express.text({ type: 'text/plain' }));
